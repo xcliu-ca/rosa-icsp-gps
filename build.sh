f63b781c1e066602864eca3b4cdcd98b74cce4a3
Xinchun Liu <xcliu@ca.ibm.com>
2023-07-07 12:53:17 -0400

    build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ podman manifest rm lospringliu/roks-enabler:rosa  || true
 sleep 3
 podman build --platform linux/arm64 --platform linux/amd64 --manifest lospringliu/roks-enabler:rosa .
 sleep 3
-podman manifest push -f v2s2 lospringliu/roks-enabler:latest quay.io/cicdtest/roks-enabler:rosa
-podman manifest push -f v2s2 lospringliu/roks-enabler:latest docker.io/lospringliu/roks-sync:rosa
+podman manifest push -f v2s2 lospringliu/roks-enabler:rosa quay.io/cicdtest/roks-enabler:rosa
+podman manifest push -f v2s2 lospringliu/roks-enabler:rosa docker.io/lospringliu/roks-sync:rosa
